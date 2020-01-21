803171acb89b70e3a21495da72c7b6c14dbdb185
harib128 <tomshaddock@googlemail.com>
2020-01-21 20:42:26 +0100

    Define the migration file for db tables

--- database/migrations/2020_01_21_193905_create_items_table.php
+++ database/migrations/2020_01_21_193905_create_items_table.php
@@ -15,6 +15,8 @@ class CreateItemsTable extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('text');
+            $table->string('body');
             $table->timestamps();
         });
     }
