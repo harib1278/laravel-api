33d7be0c1ee1916df33c7cadedf94e0e87729307
harib128 <tomshaddock@googlemail.com>
2020-01-22 08:27:30 +0100

    Addition of PUT and DELETE methods to the controller

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -21,16 +21,6 @@ class ItemsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -70,16 +60,6 @@ class ItemsController extends Controller
         return response()->json($item);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -90,7 +70,23 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+      $validator = Validator::make($request->all(), [
+          'text'  => 'required',
+          'body'  => 'required',
+      ]);
+
+      if ($validator->fails()) {
+          return ['response' => $validator->messages(), 'success'];
+      }
+
+      $item = Item::find($id);
+
+      $item->text = $request->input('text');
+      $item->body = $request->input('body');
+
+      $item->save();
+
+      return response()->json($item);
     }
 
     /**
@@ -101,6 +97,10 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $item = Item::find($id);
+
+        $item->destroy();
+
+        return response()->json($item);
     }
 }
